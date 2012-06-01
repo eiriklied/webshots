bca78c30bf3e76dd717f17dc26d4ccf00658b5b3
Eirik Lied <eiriklied@gmail.com>
2012-06-01 17:23:54 +0200

    A little more modularized and a nice png creating method

--- lib/webshots.rb
+++ lib/webshots.rb
@@ -1,6 +1,10 @@
 require "webshots/version"
+require "webshots/processor"
 
 module Webshots
+
+  # get an executable for the platform we are on
+  # only tested on mac and heroku
   def self.executable
     executable_file =  case RUBY_PLATFORM
       when /linux/ # heroku
@@ -10,7 +14,7 @@ module Webshots
       else
         raise "No bundled binaries found for your system."
       end
-      
+
     executable = File.absolute_path(File.join(File.dirname(__FILE__), '..', 'bin', executable_file))
   end
 end
---
+++ lib/webshots/processor.rb
@@ -0,0 +1,37 @@
+module Webshots
+  class Processor
+
+    # returns a path to a tmpfile containing a png with a screenshot
+    def self.url_to_png(url, options_param={})
+      raise "no url given" unless url
+
+      default_options = {'crop-h' => 768, 'crop-w' => 1024, 'height' => 768, 'width' => 1024}
+      options = default_options.merge(options_param)
+
+      options_str = str_from_options(options)
+
+       # Need to create a temporary file with a png extension that wkhtmltoimage can write to
+      @tmp_file_path = File.dirname(Tempfile.new('screenshot')) + rand_str + '.png'
+      
+      cmd = "#{Webshots.executable} #{options_str} #{url} #{@tmp_file_path}"
+      system(cmd)
+
+      @tmp_file_path
+    end
+
+    private
+
+    def self.rand_str(len=20)
+      rand(36**len).to_s(36)
+    end
+
+    def self.str_from_options(options={})
+      str = ''
+      options.each do |key,val|
+        str << "--#{key} #{val} "
+      end
+      str
+    end
+
+  end
+end
