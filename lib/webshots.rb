1d4db34ca55acb04f66daef3776064c967180b15
Eirik Lied <eiriklied@gmail.com>
2012-06-01 16:39:51 +0200

    dont use puts anywhere

--- lib/webshots.rb
+++ lib/webshots.rb
@@ -2,7 +2,6 @@ require "webshots/version"
 
 module Webshots
   def self.executable
-    puts "Webshots: #{__FILE__}"
     executable_file =  case RUBY_PLATFORM
       when /linux/ # heroku
         'wkhtmltoimage-amd64'
@@ -11,6 +10,7 @@ module Webshots
       else
         raise "No bundled binaries found for your system."
       end
+      
     executable = File.absolute_path(File.join(File.dirname(__FILE__), '..', 'bin', executable_file))
   end
 end
